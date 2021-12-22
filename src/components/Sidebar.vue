145a7d52302123b5265d4da4a6e2b839ea59d441
Nara <736264660@qq.com>
2021-12-22 16:31:15 +0800

    修改Sidebar样式

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <avatar></avatar>
     <div class="icons">
       <router-link to="/note/1" title="所有笔记"><i class="iconfont icon-note"></i>所有笔记</router-link>
-      <router-link to="/notebooks" title="笔记本">><i class="iconfont icon-notebook"></i>笔记本</router-link>
+      <router-link to="/notebooks" title="笔记本"><i class="iconfont icon-notebook"></i>笔记本</router-link>
       <router-link to="/trash/1" title="废纸篓"><i class="iconfont icon-trash"></i>废纸篓</router-link>
     </div>
     <div class="logout">
@@ -34,23 +34,23 @@ export default {
   a {
     padding: 6px 0;
     display: block;
+    color: white;
   }
 
   .router-link-active {
     background-color: #5e6266;
   }
+}
 
-  .logout {
-    position: absolute;
-    bottom: 20px;
-    width: 100%;
-    text-align: center;
-    cursor: pointer;
-  }
+.logout {
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+}
 
-  .iconfont {
-    color: #fff;
-  }
-
+.iconfont {
+  color: #fff;
 }
 </style>
