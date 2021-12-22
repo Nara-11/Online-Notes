b4fd6a2c0c73bc2c5eac5a3b48d0b4fe96f6d911
Nara <736264660@qq.com>
2021-12-22 15:30:03 +0800

    开始项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <sidebar></sidebar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import sidebar from './components/Sidebar.vue'
+export default {
+  name: 'App',
+  components:{sidebar}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
